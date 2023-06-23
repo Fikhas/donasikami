3bb216857e611d8b7fe3949ce1ad5146e354ebec
Fikhas <fikrihasan050@gmail.com>
2023-06-23 10:07:52 +0700

    add model and utils

---
+++ model/modelsdb.js
@@ -0,0 +1,86 @@
+const mongoose = require('mongoose')
+
+const articleSchema = new mongoose.Schema({
+    image: {
+        type: String,
+        required: true
+    },
+    article: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+}, {
+    timestamps: true
+})
+
+const Donatur = mongoose.model('Donatur', {
+    name: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+
+    address: {
+        type: String,
+        required: true
+    },
+    stuffType: {
+        type: String,
+        required: true
+    },
+    conditionType: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        required: true
+    },
+    time: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    }
+
+});
+
+const Account = mongoose.model('Account', {
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+const Article = mongoose.model('Article', articleSchema)
+
+// const contact1 = new Contact({
+//     nama: 'Rifqi',
+//     noHP: '087109274018',
+//     email: 'rifqiy@gmail.com'
+// });
+
+// contact1.save().then((contact) => console.log(contact));
+
+module.exports = {Donatur, Account, Article};
---
+++ utils/db.js
@@ -0,0 +1,3 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://fikrihasan_30:ZOrcfbvr9ogYFBSJ@cluster0.zr5xf.mongodb.net/', {
+});
