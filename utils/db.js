30450616c58fcc15ada28d236a76027d02bbbc5f
Fikhas <fikrihasan050@gmail.com>
2023-06-24 09:24:56 +0700

    app changed

--- utils/db.js
+++ utils/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const localDB = "mongodb://127.0.0.1:27017/donasiKami"
 const dev_db_url =
-  "mongodb+srv://fikrihasan_30:ZOrcfbvr9ogYFBSJ@cluster0.zr5xf.mongodb.net/donasiKami?retryWrites=true&w=majority";
+  "mongodb+srv://fikrihasan_30:ZOrcfbvr9ogYFBSJ@cluster0.zr5xf.mongodb.net/donasiKami";
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, {});
 // Set up mongoose connection
