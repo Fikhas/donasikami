e83e16ce675b31a6fcaa69283566123b0dc09ef9
Fikhas <fikrihasan050@gmail.com>
2023-07-04 21:20:54 +0700

    bin changed

--- bin/www.js
+++ bin/www.js
@@ -85,5 +85,5 @@ function onListening() {
   var bind = typeof addr === 'string'
     ? 'pipe ' + addr
     : 'port ' + addr.port;
-  console.log(`Listening on: http://localhost:${port}`);
+  debug(`Listening on ${bind}`);
 }
