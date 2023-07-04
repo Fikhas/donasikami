77fc9764ec8f9cc59f11e7ded3f41d45a3971551
Fikhas <fikrihasan050@gmail.com>
2023-07-04 07:54:03 +0700

    script changed

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,8 +1,6 @@
-// const nav = document.querySelector("nav");
+const nav = document.querySelector("nav");
 
-// if(window.scrollY >= 100) {
-//     nav.style.backgroundColor = "black";
-//     console.log("Oke");
-// }
-
-// console.log(window.scrollY);
+if(window.scrollY >= 100) {
+    nav.style.backgroundColor = "black";
+    console.log("Oke");
+}
