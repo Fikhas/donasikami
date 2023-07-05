257f6e6bff9de78d1145e66a316764bce01e3991
Fikhas <fikrihasan050@gmail.com>
2023-07-05 10:36:40 +0700

    connect db update

--- utils/db.js
+++ utils/db.js
@@ -4,4 +4,4 @@ const localDB = "mongodb://127.0.0.1:27017/donasiKami"
 const dev_db_url =
   "mongodb+srv://fikrihasan_30:ZOrcfbvr9ogYFBSJ@cluster0.zr5xf.mongodb.net/donasiKami";
 const mongoDB = process.env.MONGO_URI || dev_db_url;
-mongoose.connect(localDB);
+mongoose.connect(mongoDB);
